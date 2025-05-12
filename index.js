a677e58650ae606be470dda6adebe3acd0f48c9a
Andrey web2025-6 <malyi.andrew@gmail.com>
2025-05-12 03:34:31 +0300

    Implemented CLI options and server start

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Server stub");
+const express = require('express');
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const { program } = require('commander');
+
+program
+  .requiredOption('-h, --host <host>', 'Server host')
+  .requiredOption('-p, --port <port>', 'Server port')
+  .requiredOption('-c, --cache <dir>', 'Cache directory');
+
+program.parse(process.argv);
+const options = program.opts();
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+server.listen(options.port, options.host, () => {
+  console.log(`Server running at http://${options.host}:${options.port}`);
+});
+
